1e7fe7a253119db51d3920e24859ea8711e6ba84
Nic Hannah <nicjhannah@gmail.com>
2016-05-12 22:03:12 +1000

    Update model build.

--- model.py
+++ model.py
@@ -6,9 +6,6 @@ import os
 import subprocess as sp
 import shlex
 
-_file_dir = os.path.dirname(os.path.abspath(__file__))
-_build_dir = os.path.normpath(os.path.join(_file_dir, '../../build'))
-
 _build_fms_script = """
 ../../../../mkmf/bin/list_paths ../../../../src/FMS &&
 ../../../../mkmf/bin/mkmf -t ../../../../mkmf/templates/{site}-{compiler}.mk -p libfms.a -c "-Duse_libMPI -Duse_netCDF -DSPMD" path_names &&
@@ -17,16 +14,16 @@ source ../../../../mkmf/env/{site}-{compiler}.env && make NETCDF=3 {build}=1 lib
 
 _build_ocean_script = """
 pwd &&
-../../../../mkmf/bin/list_paths ./ ../../../../src/MOM6/{{config_src/dynamic_symmetric,config_src/solo_driver,src/{{*,*/*}}}}/
-../../../../mkmf/bin/mkmf -t ../../../../mkmf/templates/{site}-{compiler}.mk -o '-I../../shared/{build}' -p 'MOM6 -L../../shared/{build} -lfms' -c "-Duse_libMPI -Duse_netCDF -DSPMD" path_names &&
-source ../../../../mkmf/env/{site}-{compiler}.env && make NETCDF=3 {build}=1 MOM6 -j
+../../../../../mkmf/bin/list_paths ./ ../../../../../src/MOM6/{{config_src/{memory_type},config_src/solo_driver,src/{{*,*/*}}}}/
+../../../../../mkmf/bin/mkmf -t ../../../../../mkmf/templates/{site}-{compiler}.mk -o '-I../../../shared/{build}' -p 'MOM6 -L../../../shared/{build} -lfms' -c "-Duse_libMPI -Duse_netCDF -DSPMD" path_names &&
+source ../../../../../mkmf/env/{site}-{compiler}.env && make NETCDF=3 {build}=1 MOM6 -j
 """
 
 _build_ocean_ice_script = """
 pwd &&
-../../../../mkmf/bin/list_paths ./ ../../../../src/MOM6/config_src/{{dynamic_symmetric,coupled_driver}} ../../../../src/MOM6/src/{{*,*/*}}/ ../../../../src/{{atmos_null,coupler,land_null,ice_param,SIS2,FMS/coupler,FMS/include}} &&
-../../../../mkmf/bin/mkmf -t ../../../../mkmf/templates/{site}-{compiler}.mk -o '-I../../shared/{build}' -p 'MOM6 -L../../shared/{build} -lfms' -c '-Duse_libMPI -Duse_netCDF -DSPMD -DUSE_LOG_DIAG_FIELD_INFO' path_names &&
-source ../../../../mkmf/env/{site}-{compiler}.env && make NETCDF=3 {build}=1 MOM6 -j
+../../../../../mkmf/bin/list_paths ./ ../../../../../src/MOM6/config_src/{{{memory_type},coupled_driver}} ../../../../../src/MOM6/src/{{*,*/*}}/ ../../../../../src/{{atmos_null,coupler,land_null,ice_param,SIS2,FMS/coupler,FMS/include}} &&
+../../../../../mkmf/bin/mkmf -t ../../../../../mkmf/templates/{site}-{compiler}.mk -o '-I../../../shared/{build}' -p 'MOM6 -L../../../shared/{build} -lfms' -c '-Duse_libMPI -Duse_netCDF -DSPMD -DUSE_LOG_DIAG_FIELD_INFO' path_names &&
+source ../../../../../mkmf/env/{site}-{compiler}.env && make NETCDF=3 {build}=1 MOM6 -j
 """
 
 def mkdir_p(path):
@@ -40,11 +37,13 @@ def mkdir_p(path):
 
 class Model:
 
-    def __init__(self, name):
+    def __init__(self, name, mom_dir):
         self.name = name
+        self.mom_dir = mom_dir
+        self.build_dir = os.path.join(self.mom_dir, 'build')
         self.site = 'raijin'
 
-    def build(self, build_kind, compiler):
+    def build(self, compiler, build, memory_type):
         """
         Build this model.
         """
@@ -52,7 +51,7 @@ class Model:
         ret = 0
 
         # Build FMS
-        shared_dir = os.path.join(_build_dir, compiler, 'shared', build_kind)
+        shared_dir = os.path.join(self.build_dir, compiler, 'shared', build)
         mkdir_p(shared_dir)
         os.chdir(shared_dir)
         command = _build_fms_script.format(site=self.site, build=build_kind,
@@ -69,15 +68,17 @@ class Model:
             return ret
 
         # Build either ocean_only or ice and ocean.
-        model_dir = os.path.join(_build_dir, compiler, self.name, build_kind)
+        model_dir = os.path.join(_build_dir, compiler, self.name, build, memory_type)
         mkdir_p(model_dir)
         os.chdir(model_dir)
         if self.name == 'ocean_only':
-            command = _build_ocean_script.format(site=self.site, build=build_kind,
-                                                 compiler=compiler)
+            command = _build_ocean_script.format(site=self.site, build=build,
+                                                 compiler=compiler,
+                                                 memory_type=memory_type)
         else:
-            command = _build_ocean_ice_script.format(site=self.site, build=build_kind,
-                                                     compiler=compiler)
+            command = _build_ocean_ice_script.format(site=self.site, build=build,
+                                                     compiler=compiler,
+                                                     memory_type=memory_type)
         try:
             output = sp.check_output(command, stderr=sp.STDOUT, shell=True)
         except sp.CalledProcessError as e:
